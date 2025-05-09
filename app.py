7bb9aa38f3ff9624e0b5b87bcb24c294a64bb05a
Maham Waseem <mahamwaseem316@gmail.com>
2025-05-09 15:46:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -504,4 +504,5 @@ def server_error(error):
     return render_template('errors/500.html'), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)  # Explicit host/port binding
