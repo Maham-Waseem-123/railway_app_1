27d922eda5155d84237f9de0ec1e09aa851eb085
Maham Waseem <mahamwaseem316@gmail.com>
2025-05-09 16:44:23 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0:8000 --timeout 600 app:app
+
+# Activate the virtual environment
+source /antenv/bin/activate
+
+# Install any missing dependencies
+pip install -r requirements.txt
+
+# Run migrations if needed
+flask db upgrade
+
+# Start Gunicorn
+gunicorn --bind=0.0.0.0:${PORT:-8000} --timeout 600 app:app
